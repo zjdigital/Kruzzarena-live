58c6b95ec02fb2c483add298efa5eb2ff7e8f9de
zjdigital <zjdigital01@gmail.com>
2026-06-18 08:06:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 window.KRUZZARENA_CONFIG = {
   adminPassword: "12345",
 
-  firebase: {
-    apiKey: "ISI_API_KEY",
-    authDomain: "ISI_PROJECT.firebaseapp.com",
-    databaseURL: "https://ISI_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "ISI_PROJECT",
-    storageBucket: "ISI_PROJECT.appspot.com",
-    messagingSenderId: "ISI_SENDER_ID",
-    appId: "ISI_APP_ID"
-  }
+  apiKey: "AIzaSyAkJuxPgQO5Zoe3cIfYAmgqOUV2yVq38S4",
+  authDomain: "kruzzarena-3589c.firebaseapp.com",
+  projectId: "kruzzarena-3589c",
+  storageBucket: "kruzzarena-3589c.firebasestorage.app",
+  messagingSenderId: "653542221594",
+  appId: "1:653542221594:web:b6faa1188490fc36d7aea3",
+  measurementId: "G-EHL05SGNZF"
 };
+
