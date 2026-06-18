d3c1f054304967e9799053f05cb911809834bb36
zjdigital <zjdigital01@gmail.com>
2026-06-18 08:11:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 window.KRUZZARENA_CONFIG = {
   adminPassword: "12345",
 
-  apiKey: "AIzaSyAkJuxPgQO5Zoe3cIfYAmgqOUV2yVq38S4",
-  authDomain: "kruzzarena-3589c.firebaseapp.com",
-  projectId: "kruzzarena-3589c",
-  storageBucket: "kruzzarena-3589c.firebasestorage.app",
-  messagingSenderId: "653542221594",
-  appId: "1:653542221594:web:b6faa1188490fc36d7aea3",
-  measurementId: "G-EHL05SGNZF"
+  firebase: {
+    apiKey: "AIzaSyAkJuxPgQO5Zoe3cIfYAmgqOUV2yVq3854",
+    authDomain: "kruzzarena-3589c.firebaseapp.com",
+    databaseURL: "https://kruzzarena-3589c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kruzzarena-3589c",
+    storageBucket: "kruzzarena-3589c.firebasestorage.app",
+    messagingSenderId: "653542221594",
+    appId: "1:653542221594:web:b6faa1188490fc36d7aea3"
+  }
 };
-
